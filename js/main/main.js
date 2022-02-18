36d9e8dd25b084e5652db90ea3e884aa472a9576
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-02-18 11:23:55 +0900

    Complete Sign In part

--- js/main/main.js
+++ js/main/main.js
@@ -11,23 +11,6 @@ const TYPING = function(){
 };
 const SPEED = setInterval(TYPING, 200);
 
-// Sign Pop Up Open handler
-// BTN
-const SIGN_OPEN = document.querySelector('#sign__open');
-const SIGN_CLOSE = document.querySelector('#sign__close');
-// SIGN BG
-const SIGN_BG = document.querySelector('.sign__bg');
-
-// SIGN OPEN
-SIGN_OPEN.addEventListener("click", () =>{
-    SIGN_BG.classList.add('sign__bg-active');
-});
-
-// SIGN CLOSE
-SIGN_CLOSE.addEventListener("click", () =>{
-    SIGN_BG.classList.remove('sign__bg-active');
-});
-
 // Privacy Policy
 const PRIVACY_OPEN = document.querySelector('#privacy');
 const PRIVACY_CLOSE = document.querySelector('#privacy__close');
---
+++ js/sign/sign.js
@@ -0,0 +1,34 @@
+// Sign Pop Up Open handler
+// SIGN BG
+const SIGN_BG = document.querySelector('.sign__bg');
+// BTN
+const SIGN_OPEN = document.querySelector('#sign__open');
+const SIGN_CLOSE = document.querySelector('#sign__close');
+// SIGN CONTENTS
+const SIGN_UP = document.querySelector('.signUp');
+const SIGN_IN = document.querySelector('.signIn');
+
+// SIGN OPEN
+SIGN_OPEN.addEventListener("click", () =>{
+    SIGN_BG.classList.add('sign__bg-active');
+});
+
+// SIGN CLOSE
+SIGN_CLOSE.addEventListener("click", () =>{
+    SIGN_BG.classList.remove('sign__bg-active');
+    SIGN_UP.classList.remove('signUp-stop');
+    SIGN_IN.classList.remove('signIn-active');
+});
+// Sign In open handler
+const SIGN_IN_BTN = document.querySelector('#signIn__btn');
+const RETURN_TO_SIGN_UP = document.querySelector('#signUp__move__btn');
+
+SIGN_IN_BTN.addEventListener("click", () => {
+    SIGN_UP.classList.add('signUp-stop');
+    SIGN_IN.classList.add('signIn-active');
+});
+
+RETURN_TO_SIGN_UP.addEventListener("click", ()=>{
+    SIGN_UP.classList.remove('signUp-stop');
+    SIGN_IN.classList.remove('signIn-active');
+});
