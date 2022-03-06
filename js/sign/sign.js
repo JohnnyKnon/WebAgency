bdb72f707161c6bb56ef244fdacfb46a586adff9
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-03-06 23:00:33 +0900

    Modify sign In effect / web.html apply part complete

--- js/sign/sign.js
+++ js/sign/sign.js
@@ -9,26 +9,31 @@ const SIGN_UP = document.querySelector('.signUp');
 const SIGN_IN = document.querySelector('.signIn');
 
 // SIGN OPEN
-SIGN_OPEN.addEventListener("click", () =>{
-    SIGN_BG.classList.add('sign__bg-active');
+SIGN_OPEN.addEventListener('click', () => {
+  SIGN_BG.classList.add('sign__bg-active');
+  //   loader
+  document.body.classList.add('reveal');
 });
 
 // SIGN CLOSE
-SIGN_CLOSE.addEventListener("click", () =>{
-    SIGN_BG.classList.remove('sign__bg-active');
-    SIGN_UP.classList.remove('signUp-stop');
-    SIGN_IN.classList.remove('signIn-active');
+SIGN_CLOSE.addEventListener('click', () => {
+  SIGN_BG.classList.remove('sign__bg-active');
+  SIGN_UP.classList.remove('signUp-stop');
+  SIGN_IN.classList.remove('signIn-active');
+  //   loader
+  document.body.classList.remove('reveal');
 });
+
 // Sign In open handler
 const SIGN_IN_BTN = document.querySelector('#signIn__btn');
 const RETURN_TO_SIGN_UP = document.querySelector('#signUp__move__btn');
 
-SIGN_IN_BTN.addEventListener("click", () => {
-    SIGN_UP.classList.add('signUp-stop');
-    SIGN_IN.classList.add('signIn-active');
+SIGN_IN_BTN.addEventListener('click', () => {
+  SIGN_UP.classList.add('signUp-stop');
+  SIGN_IN.classList.add('signIn-active');
 });
 
-RETURN_TO_SIGN_UP.addEventListener("click", ()=>{
-    SIGN_UP.classList.remove('signUp-stop');
-    SIGN_IN.classList.remove('signIn-active');
+RETURN_TO_SIGN_UP.addEventListener('click', () => {
+  SIGN_UP.classList.remove('signUp-stop');
+  SIGN_IN.classList.remove('signIn-active');
 });
