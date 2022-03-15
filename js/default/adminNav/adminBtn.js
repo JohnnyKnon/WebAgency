e58f2f7cf64f9dc2490330874f46b51d1f444e98
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-03-15 12:19:12 +0900

    Hosting apply page complete

--- js/default/adminNav/adminBtn.js
+++ js/default/adminNav/adminBtn.js
@@ -4,9 +4,11 @@ const ADMIN_TOGGLE_BTN = document.querySelector('.admin__toggle__btn');
 const OPEN_ICON = document.querySelector('.open');
 const CLOSE_ICON = document.querySelector('.close');
 const ADMIN_NAV_TOTAL_BOX = document.querySelector('.admin');
+const BUTTONS_WRAP = document.querySelector('.buttons');
 
 ADMIN_TOGGLE_BTN.addEventListener('click', () => {
   ADMIN_NAV_TOTAL_BOX.classList.toggle('admin-active');
   OPEN_ICON.classList.toggle('open-hidden');
   CLOSE_ICON.classList.toggle('close-active');
+  BUTTONS_WRAP.classList.toggle('buttons-active');
 });
--- js/default/scrollBtn/scrollBtn.js
+++ js/default/scrollBtn/scrollBtn.js
@@ -5,6 +5,11 @@ const UP_BTN = document.querySelector('.up');
 UP_BTN.addEventListener('click', () => {
   scrollIntoView('#home');
 });
+// Down Button
+const DOWN_BTN = document.querySelector('.down');
+DOWN_BTN.addEventListener('click', () => {
+  scrollIntoView('#footer__box');
+});
 
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector); //scrollIntoView엘리멘트 의 부모 컨테이너로 스크롤되는 메소드
